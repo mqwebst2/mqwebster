d390561fa8a7b63a4fed067b9ff02f2f00231452
Marques Webster <marques.q.webster@gmail.com>
2022-10-31 17:06:17 -0500

    file formatting change

---
+++ src/js/components/button-alt.js
@@ -0,0 +1,30 @@
+import '/src/css/components/button-alt.css';
+
+class CustomButtonAlt extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const link = this.getAttribute('btn-link') || '#';
+    const text = this.getAttribute('btn-text');
+    const icon = this.getAttribute('btn-icon');
+
+    this.innerHTML = `
+      <div class="custom-btn-alt">
+        <a href="${link}" target="_blank" rel="noopener noreferrer" class="page-btn-alt custom-btn-alt__link">
+          <span class="custom-btn-alt__link-text">${text}</span>
+          <div class="custom-btn-alt__link-img">
+            <img
+              src="${icon}"
+              alt="Button Icon"
+              class="custom-btn-alt__link-img--inner"
+            />
+          </div>
+        </a>
+      </div>
+    `;
+  }
+}
+
+customElements.define('custom-btn-alt', CustomButtonAlt);
---
+++ src/js/components/button.js
@@ -0,0 +1,50 @@
+import '/src/css/components/button.css';
+
+class CustomButton extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const link = this.getAttribute('btn-link') || '#';
+    const text = this.getAttribute('btn-text');
+    const icon = this.getAttribute('btn-icon');
+
+    this.innerHTML = `
+      <div class="custom-btn">
+        <a href="${link}" target="_blank" rel="noopener noreferrer" class="page-btn custom-btn__link">
+          <span class="custom-btn__link-text">${text}</span>
+          <div class="custom-btn__link-img">
+            <img
+              src="${icon}"
+              alt="Button Icon"
+              class="custom-btn__link-img--inner"
+            />
+          </div>
+        </a>
+      </div>
+    `;
+
+    const mailtoText = 'mailto:';
+
+    if (link.includes(mailtoText)) {
+      const button = this.querySelector('.custom-btn__link');
+      const buttonText = this.querySelector('.custom-btn__link-text');
+
+      button.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        this.link = link.replace('mailto:', '');
+        navigator.clipboard.writeText(this.link);
+
+        buttonText.textContent = 'Copied!';
+
+        setTimeout(() => {
+          buttonText.textContent = 'Email';
+        }, 2000);
+      });
+    }
+  }
+}
+
+customElements.define('custom-btn', CustomButton);
