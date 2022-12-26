780ac08e40799594d7e0366b323ddb6612995925
Marques Webster <marques.q.webster@gmail.com>
2022-12-26 17:52:13 -0600

    reverted changes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import resolve from '@rollup/plugin-node-resolve';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import json from '@rollup/plugin-json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    resolve({
-      browser: true,
-      preferBuiltins: false,
-      dedupe: ['react'],
-    }),
-    json(),
-  ],
+  plugins: [react()],
   base: '/',
 });
