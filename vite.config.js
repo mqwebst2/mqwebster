c6c6ea971e2c270df1a2b3be33c6c7ea8cfec09e
Marques Webster <marques.q.webster@gmail.com>
2022-12-26 15:46:34 -0600

    build changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dist',
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
