a1728c189ad4bca45a03392cb4dd22d793701e0e
Marques Webster <marques.q.webster@gmail.com>
2022-12-27 22:14:50 -0600

    importing images into resume

---
+++ src/components/data/resumeImages.js
@@ -0,0 +1,27 @@
+import CSS from '/src/assets/logos/CSS.png';
+import Figma from '/src/assets/logos/Figma.png';
+import GitHub from '/src/assets/logos/GitHub.png';
+import HTML from '/src/assets/logos/HTML.png';
+import JavaScript from '/src/assets/logos/JavaScript.png';
+import NPM from '/src/assets/logos/NPM.png';
+import Prettier from '/src/assets/logos/Prettier.png';
+import PWA from '/src/assets/logos/PWA.png';
+import ReactLogo from '/src/assets/logos/React.png';
+import Vite from '/src/assets/logos/Vite.png';
+import Webflow from '/src/assets/logos/Webflow.png';
+import WordPress from '/src/assets/logos/WordPress.png';
+
+export default [
+  { url: CSS },
+  { url: Figma },
+  { url: GitHub },
+  { url: HTML },
+  { url: JavaScript },
+  { url: NPM },
+  { url: Prettier },
+  { url: PWA },
+  { url: ReactLogo },
+  { url: Vite },
+  { url: Webflow },
+  { url: WordPress },
+];
--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -2,80 +2,83 @@ import React from 'react';
 import ResumeItem from '../components/js/ResumeItem';
 import data from '../components/data/resumeData.js';
 import skillIcon from '/src/assets/icons/skill-icon.png';
+import images from '/src/components/data/resumeImages.js';
 
 export default function Resume() {
   const educationElements = data.Education.map((item) => {
-    return <ResumeItem key={item.id} section='Education' {...item} />;
+    return <ResumeItem key={item.id} section="Education" {...item} />;
   });
   const experienceElements = data.Experience.map((item) => {
-    return <ResumeItem key={item.id} section='Experience' {...item} />;
+    return <ResumeItem key={item.id} section="Experience" {...item} />;
   });
 
   const skills = [
-    'CSS',
-    'Figma',
-    'GitHub',
-    'HTML',
-    'JavaScript',
-    'NPM',
-    'Prettier',
-    'PWA',
-    'React',
-    'Vite',
-    'Webflow',
-    'WordPress',
+    { id: 1, name: 'CSS', url: '' },
+    { id: 2, name: 'Figma', url: '' },
+    { id: 3, name: 'GitHub', url: '' },
+    { id: 4, name: 'HTML', url: '' },
+    { id: 5, name: 'JavaScript', url: '' },
+    { id: 6, name: 'NPM', url: '' },
+    { id: 7, name: 'Prettier', url: '' },
+    { id: 8, name: 'PWA', url: '' },
+    { id: 9, name: 'React', url: '' },
+    { id: 10, name: 'Vite', url: '' },
+    { id: 11, name: 'Webflow', url: '' },
+    { id: 12, name: 'WordPress', url: '' },
   ];
 
-  const skillsElement = skills.map((item) => {
+  const skillsElement = skills.map((item, i) => {
+    console.log(i);
+    item.url = images[i].url;
     return (
-      <li key={item}>
+      <li key={item.id}>
         <img
-          src={`/src/assets/logos/${item}.png`}
-          alt={`${item} Logo`}
-          className='hard-skills__icon'
+          src={item.url}
+          alt={`${item.name} Logo`}
+          className="hard-skills__icon"
         />
-        {item}
+        {item.name}
       </li>
     );
   });
 
   return (
-    <section id='resume'>
-      <div className='wrap'>
+    <section id="resume">
+      <div className="wrap">
         <h1>My Experience...</h1>
 
-        <div className='resume-wrap'>
-          <div className='resume-nav'>
-            <ul className='resume-nav__items'>
+        <div className="resume-wrap">
+          <div className="resume-nav">
+            <ul className="resume-nav__items">
               <li>
-                <a href=''>Education</a>
+                <a href="">Education</a>
               </li>
               <li>
-                <a href=''>Skills</a>
+                <a href="">Skills</a>
               </li>
               <li>
-                <a href=''>Experience</a>
+                <a href="">Experience</a>
               </li>
             </ul>
           </div>
 
-          <div className='resume-content'>
-            <div className='resume-section' id='Education'>
+          <div className="resume-content">
+            <div className="resume-section" id="Education">
               <h2>Education</h2>
               {educationElements}
             </div>
 
-            <div className='resume-section' id='Skills'>
+            <div className="resume-section" id="Skills">
               <h2>Skills</h2>
 
-              <div className='resume-item'>
-                <div className='resume-item__icon'>
-                  <img src={skillIcon} alt='Resume Item Icon' />
+              <div className="resume-item">
+                <div className="resume-item__icon">
+                  <img src={skillIcon} alt="Resume Item Icon" />
                 </div>
 
-                <div className='resume-item__content'>
+                <div className="resume-item__content">
                   <h3>Interpersonal Skills</h3>
-                  <ul className='resume-item__list'>
+                  <ul className="resume-item__list">
                     <li>Detail-oriented work ethic</li>
                     <li>Employee resource management</li>
                     <li>
@@ -87,19 +90,19 @@ export default function Resume() {
                 </div>
               </div>
 
-              <div className='resume-item'>
-                <div className='resume-item__icon'>
-                  <img src={skillIcon} alt='Resume Item Icon' />
+              <div className="resume-item">
+                <div className="resume-item__icon">
+                  <img src={skillIcon} alt="Resume Item Icon" />
                 </div>
 
-                <div className='resume-item__content'>
+                <div className="resume-item__content">
                   <h3>Technical Compentencies</h3>
-                  <ul className='resume-item__hard-skills'>{skillsElement}</ul>
+                  <ul className="resume-item__hard-skills">{skillsElement}</ul>
                 </div>
               </div>
             </div>
 
-            <div className='resume-section' id='Experience'>
+            <div className="resume-section" id="Experience">
               <h2>Experience</h2>
 
               {experienceElements}
