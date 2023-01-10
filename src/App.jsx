030429b3d37dbc5b0c6ddfda82bd65cfdda1de5a
Marques Webster <marques.q.webster@gmail.com>
2023-01-09 21:58:23 -0600

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from 'react';
+import Header from './components/js/Header';
 import Home from './sections/Home';
 import About from './sections/About';
 import Resume from './sections/Resume';
 import Portfolio from './sections/Portfolio';
 import Contact from './sections/Contact';
-import Header from './components/js/Header';
 import './App.css';
 
 function App() {
   const [visitors, setVisitors] = useState('');
 
-  useEffect(() => {
-    const apiURL =
-      'https://rty7kmnenc.execute-api.us-east-2.amazonaws.com/api/siteViewCount';
+  // useEffect(() => {
+  //   const apiURL =
+  //     'https://rty7kmnenc.execute-api.us-east-2.amazonaws.com/api/siteViewCount';
 
-    fetch(apiURL)
-      .then((res) => res.json())
-      .then((data) => {
-        setVisitors(data.body);
-      });
-  }, []);
+  //   fetch(apiURL)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setVisitors(data.body);
+  //     });
+  // }, []);
 
   return (
     <div className="App">
