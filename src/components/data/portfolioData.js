4f7eff9a6de57b7c8b7af1dec81148decd527392
Marques Webster <marques.q.webster@gmail.com>
2023-02-22 12:13:32 -0600

    updated portfolioData ids

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -26,7 +26,7 @@ export default {
       type: 'project',
     },
     {
-      id: 1,
+      id: 2,
       title: 'Airbnb Experiences Clone',
       desc: 'Small clone of the AirBnb Experiences site. This was a test for importing data and dynamically displaying it on the page. This project used data mapping in Vite React to programmatically display the faux events listed.',
       link: 'https://github.com/mqwebst2/airbnb-clone',
@@ -34,7 +34,7 @@ export default {
       type: 'project',
     },
     {
-      id: 2,
+      id: 3,
       title: 'Meme Generator',
       desc: 'Looking for a quick laugh and maybe make some friends and family laugh too? Check out this meme generator I built. This was great practice for State and Effect hooks in React.',
       link: 'https://github.com/mqwebst2/meme-generator',
@@ -42,7 +42,7 @@ export default {
       type: 'project',
     },
     {
-      id: 3,
+      id: 4,
       title: 'Tenzi Dice Game',
       desc: 'Based on the Tenzi Dice Game, this is my own online version. This game is heavy on the State management and equally so on fun factor. Feel free to try the game out until you win.',
       link: 'https://github.com/mqwebst2/tenzies-dice-game',
@@ -50,7 +50,7 @@ export default {
       type: 'project',
     },
     {
-      id: 4,
+      id: 5,
       title: 'Notes App',
       desc: 'By far the most complex project listed but it was worth the effort. This note app takes advantage of React hooks like State and Effect as well as utilizing the local storage of the browser. Try taking some notes and come back to them!',
       link: 'https://github.com/mqwebst2/notes-app',
